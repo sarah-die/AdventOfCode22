c596998cc64b4c0dc18d4f5eb194f2598488a847
sarah-die <saridi@alice.de>
2022-12-02 12:19:27 +0100

    Initial commit

---
+++ day1/star1.test.ts
@@ -0,0 +1,7 @@
+import {star1} from "./star1";
+
+describe("star 1", () => {
+    it("should return 1", () => {
+        expect(star1()).toBe(1)
+    })
+})
---
+++ day1/star1.ts
@@ -0,0 +1,3 @@
+export const star1 = () => {
+    return 1
+}
